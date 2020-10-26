d6bb05d1c0d27b558ccf2eb731caa56b16b34f3c
Donnie Plumly <donnie.plumly@gmail.com>
2020-10-26 14:05:26 -0400

    added source and medium

--- js/app.js
+++ js/app.js
@@ -5,9 +5,24 @@ function baseURL() {
 
 function utm_campaign() {
 	let campaignNumber = document.getElementsByName('campaignNumber')[0].value;
-	let utm_campaign = '/utm_campaign=' + campaignNumber;
+	let utm_campaign = '?utm_campaign=' + campaignNumber;
 	document.getElementById('utm_campaign').innerHTML = utm_campaign;
 }
+
+function utm_source() {
+	let source = document.getElementsByName('source')[0].value;
+	let utm_source = '&utm_source=' + source;
+	document.getElementById('utm_source').innerHTML = utm_source;
+}
+
+function utm_medium() {
+	let medium = document.getElementsByName('medium')[0].value;
+	let utm_medium = '&utm_medium=' + medium;
+	document.getElementById('utm_medium').innerHTML = utm_medium;
+}
+
+
+
 
 // reset form
 let reset = reset => { document.getElementById('form').reset(); }
@@ -18,12 +33,17 @@ let reset = reset => { document.getElementById('form').reset(); }
 // reset form functions
 let resetURL = resetURL => { document.getElementById('baseURL').innerHTML = ' '; }
 let resetUtmCampaign = resetUtmCampaign => { document.getElementById('utm_campaign').innerHTML = ' '; }
+let resetSourceCampaign = resetSourceCampaign => { document.getElementById('utm_source').innerHTML = ' '; }
+let resetMediumCampaign = resetMediumCampaign => { document.getElementById('utm_medium').innerHTML = ' '; }
+
 
 // fuction to reset all forms
 function resetText() {
 	reset();
 	resetURL();
 	resetUtmCampaign();
+	resetSourceCampaign();
+	resetMediumCampaign();
 }
 
 
